afffb6fa87fd638962044b20ad670e44e823299c
Mojahid <raofahmedmojahid@gmail.com>
2024-10-13 14:28:58 +0600

    addedd

--- wp-content/themes/edufax/template-parts/blog/blog-btn.php
+++ wp-content/themes/edufax/template-parts/blog/blog-btn.php
@@ -10,6 +10,7 @@
 
 $edufax_blog_btn = get_theme_mod('edufax_blog_btn', 'View Details');
 $edufax_blog_btn_switch = get_theme_mod('edufax_blog_btn_switch', true);
+$edufax_blog_comments = get_theme_mod('edufax_blog_comments', true);
 
 ?>
 
@@ -19,7 +20,12 @@ $edufax_blog_btn_switch = get_theme_mod('edufax_blog_btn_switch', true);
             <?php if (!empty($edufax_blog_btn)) : ?>
                 <a href="<?php the_permalink(); ?>"><?php echo esc_html($edufax_blog_btn); ?> <i class="far fa-long-arrow-right"></i></a>
             <?php endif; ?>
-            <span><i class="far fa-eye"></i> 80 View</span>
+            <?php if (!empty($edufax_blog_comments)) : ?>
+            <span>
+                <i class="fal fa-comment"></i>
+                <?php comments_number(); ?>
+            </span>
+            <?php endif; ?>
         </div>
     </div>
 <?php endif; ?>
